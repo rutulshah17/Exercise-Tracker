d08d24a302ea293d838722db992bf9e07d4ebd70
Rutul Shah <rutulshah17@gmail.com>
2021-04-09 00:37:26 -0400

    created routes (endpoints) - might want to create move function definition of routes under controllers folder

---
+++ server/routes/users.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import User from '../models/user.models.js'
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        console.log(users);
+        res.status(200).json(users);
+    
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.post('/add', async (req, res) => {
+    
+    const userName = req.body.username;
+    const newUser = new User(userName);
+    
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+});
+
+export default router;
